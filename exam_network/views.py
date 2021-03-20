c5922157910d09e8782d8c826d5f3d0ffdc7be3d
2397741k <2397741k@student.gla.ac.uk>
2021-03-20 23:00:49 +0000

    Updated Views for some pages
    Implemented views for
     - 404 page
     - Exams page
     - contacts page
     - logout

--- exam_network/views.py
+++ exam_network/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return render(request, 'exam_network/index.html', {})
 
-
-# TODO: view isn't used anywhere
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -16,10 +14,35 @@ def user_login(request):
         if user:
             if user.is_active:
                 login(request, user)
-                return redirect(reverse('main_page:index'))
+                return redirect(reverse('exam_network:index'))
             return HttpResponse("Your Exam Network account is disabled.")
-        print(f'Invalid login details: {username}, {password}')
+        print(f'Invalid login details: {username}')
         return HttpResponse("Invalid login details supplied.")
     else:
-        # TODO: template doesn't exist yet but very much should
+        # TODO: check the names of html files when available
         return render(request, 'exam_network/login.html')
+
+def contact(request):
+    return render(request, 'exam_network/contact.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('exam_network:index'))
+
+def show_exams(request, course_name):
+    context_dict = {}
+    try:
+        course = Course.objects.get(name=course_name)
+        exams = Exam.objects.filter(course=course)
+        context_dict['course'] = course
+        context_dict['exams'] = exams
+
+    except Category.DoesNotExist:
+        context_dict['course'] = None
+        context_dict['exams'] = None
+    # TODO: check the names of html files when available
+    return render(request, 'exam_network/exam.html', context=context_dict)
+
+def handler404(request, exception):
+        return render(request, 'exam_network/handler404.html', status=404)
