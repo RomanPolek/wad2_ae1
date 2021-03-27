0c419a43dc1d561d05d399a37a3561dd4e22d12a
Roman Polek <polekroman@gmail.com>
2021-03-27 18:50:42 +0000

    added login

--- exam_network/urls.py
+++ exam_network/urls.py
@@ -6,4 +6,5 @@ app_name = 'exam_network'
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),
 ]
